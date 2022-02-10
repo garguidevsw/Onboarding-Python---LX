e5c5092ae1dcdca828050d639559d385a972b230
Julio Cesar Garcia Guillen <garguidevsw@gmail.com>
2022-02-10 02:18:11 -0600

    Onboarding Python - Primer commit

---
+++ 01-python/example1.py
@@ -0,0 +1,16 @@
+# sum = 1 + 2
+# product = sum * 2
+# print(product)
+
+# # Importamos la biblioteca 
+# from datetime import date
+
+# # Obtenemos la fecha de hoy
+# date.today()
+
+# # Mostramos la fecha en la consola
+# print(date.today())
+
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
